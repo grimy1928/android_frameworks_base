5528214a020311df5d0fc8e74e632b76e1913b6a
grimy1928 <jonesadam@thurstable.co.uk>
2017-06-14 17:05:22 +0100

    Wakeblock part 2
    This is one of three parts of what is needed to get wakeblock working. My xda username is DEVILOPS 007 if you want to contact me.

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -19891,6 +19891,9 @@ Slog.v(TAG, ":: stepped forward, applying functor at tag " + parser.getName());
         }
 
         Slog.v(TAG, "reconcileAppsData finished " + preparedCount + " packages");
+        // @ WAKEBLOCK
+        com.giovannibozzano.wakeblock.WakeBlockService.injectWakeBlock();
+        // # WAKEBLOCK
     }
 
     /**
